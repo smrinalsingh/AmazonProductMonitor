01019ba2571fa820224d00da6ee6891fcbe3ad43
smrinalsingh <s.mrinalsingh@gmail.com>
2020-06-07 16:13:55 +0530

    Adapted for cli with url as param1.

--- AmazonProductMonitor.bat
+++ AmazonProductMonitor.bat
@@ -1,5 +1,5 @@
 @echo off & setlocal
 
 echo Invoking Amazon Monitor
-cmd /c "python.exe AmazonProductMonitor.py"
+cmd /c "python.exe AmazonProductMonitor.py ^"%1^""
 notify-run send "Monitor Stopped"
