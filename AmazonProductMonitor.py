f14d826c8d0224a1495f2f80f5abcd7b8f81f455
smrinalsingh <s.mrinalsingh@gmail.com>
2020-06-07 16:09:42 +0530

    Adapted for cli with url as param1.

--- AmazonProductMonitor.py
+++ AmazonProductMonitor.py
@@ -3,24 +3,33 @@ import requests as req
 from notify_run import Notify
 from datetime import datetime
 import time
+import sys
 
-conn = req.get("https://www.amazon.in/gp/offer-listing/B07ZPM2BVR/ref=dp_olp_new_mbc?ie=UTF8&condition=new")
+url = sys.argv[1]
+
+if (url == ""):
+    url = "https://www.amazon.in/gp/offer-listing/B07ZPM2BVR/ref=dp_olp_new_mbc?ie=UTF8&condition=new"
+
+conn = req.get(url)
 soup = bs4.BeautifulSoup(conn.text,features="html.parser")
 allCostElems = soup.findAll(attrs={'class' : 'a-size-large a-color-price olpOfferPrice a-text-bold'})
 prodHead = soup.findAll(attrs={'class' : 'a-size-large a-spacing-none'})[0].text.split("\n")[1].strip()
 
 allCosts = []
 notify = Notify()
+threshold = 20500.0
 
 while (True):
     try:
+
         for costElement in allCostElems:
             cost = (float)(costElement.text.strip("Rs. ").strip("\xa0").split("Rs. ")[1].replace(",",""))
             allCosts.append(cost)
             allCosts.sort()
-            print ("lowest cost : " + str(allCosts[0]))
+            print ("Lowest Cost : %s. Threshold %s. Time: %s"%(str(allCosts[0]), 
+                (str(threshold)), datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
             
-            if (cost < 20500.0):
+            if (cost < threshold):
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 notify.send("Price alert! Time: " + timestamp + " Price: " + str(cost)
                             + " | " + prodHead)
